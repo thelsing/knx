87828bd530234c6b418e1ad6cce91c093059652f
Maggyver <46162338+Maggyver@users.noreply.github.com>
2019-05-27 20:13:57 +0200

    circuit of the led adapted for different boards (#15)
    * Update knx_facade.cpp
    
    * Update knx_facade.h
    
    * Update knx-demo.ino

--- src/knx_facade.h
+++ src/knx_facade.h
@@ -3,15 +3,12 @@
 #ifdef ARDUINO_ARCH_SAMD
 #include "samd_platform.h"
 #include "knx/bau07B0.h"
-#elif ARDUINO_ARCH_ESP8266
-#include "esp_platform.h"
-#include "knx/bau57B0.h"
-#else
-#include "linux_platform.h"
-#include "knx/bau57B0.h"
-#define LED_BUILTIN 0
 #endif
 
+#ifdef ARDUINO_ARCH_ESP8266
+#include "esp_platform.h"
+#include "knx/bau57B0.h"
+#endif
 
 
 typedef uint8_t* (*saveRestoreCallback)(uint8_t* buffer);
@@ -25,13 +22,20 @@ public:
     bool progMode();
     void progMode(bool value);
     bool configured();
+	bool ledPinActiveOn();
+	/**
+     * @brief To adapt the output to hardware.
+	 *
+	 * @param ledPinActiveOn = "0" or "low"  --> GPIO--LED--RESISTOR--VDD (for example NODE MCU)
+	 * @param ledPinActiveOn = "1" or "high" --> GPIO--RESISTOR--LED--GND (for example WeMos D1 R2)
+     */
+    void ledPinActiveOn(bool value);
     uint32_t ledPin();
     void ledPin(uint32_t value);
     uint32_t buttonPin();
     void buttonPin(uint32_t value);
     void readMemory();
     void writeMemory();
-    uint16_t induvidualAddress();
     void loop();
     void manufacturerId(uint16_t value);
     void bauNumber(uint32_t value);
@@ -48,6 +52,7 @@ public:
     GroupObject& getGroupObject(uint16_t goNr);
 private:
     BauSystemB& _bau;
+	bool _ledPinActiveOn = 0;
     uint32_t _ledPin = LED_BUILTIN;
     uint32_t _buttonPin = 0;
 #ifdef USE_STATES
@@ -60,6 +65,4 @@ private:
     uint8_t* restore(uint8_t* buffer);
 };
 
-#ifndef __linux__
 extern KnxFacade knx;
-#endif
