3193b15aabf50b6050557fd5faf3f51ffe134030
Thomas Kunze <thomas.kunze@gmx.com>
2019-06-25 22:51:52 +0200

    .

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -7,13 +7,13 @@ breathe_projects = {}
 if read_the_docs_build:
 	subprocess.call('cd .. ; doxygen', shell=True)
 	breathe_projects['knx'] = '../build/xml'
-#	html_extra_path = ['../build/html']
+	html_extra_path = ['../build/html']
 
 # -- Project information -----------------------------------------------------
 
-project = 'knx'
-copyright = '2019, Thomas Kunze'
-author = 'Thomas Kunze'
+#project = 'knx'
+#copyright = '2019, Thomas Kunze'
+#author = 'Thomas Kunze'
 
 
 # -- General configuration ---------------------------------------------------
@@ -23,17 +23,17 @@ author = 'Thomas Kunze'
 # ones.
 #...
 
-extensions = [ "breathe" ]
+#extensions = [ "breathe" ]
 
 #...
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+#templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+#exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -41,12 +41,12 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # Breathe Configuration
-breathe_default_project = "knx"
+#breathe_default_project = "knx"
--- doc/sphinx/contents.rst
+++ doc/sphinx/contents.rst
@@ -1,11 +1,2 @@
 .. toctree::
  :maxdepth: 2
- :caption: Contents:
-
-:ref:`genindex`
-
-Docs
-====
-
-.. doxygenclass:: GroupObject
-   :members:
