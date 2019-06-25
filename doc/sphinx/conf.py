d3618a7973009cbd6e8f9b19b9cc6fdfb30f4d3a
Thomas Kunze <thomas.kunze@gmx.com>
2019-06-25 22:18:19 +0200

    test breathe

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -4,4 +4,5 @@ read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
 if read_the_docs_build:
 	subprocess.call('cd .. ; doxygen', shell=True)
-	html_extra_path = ['../build/html']
+	breathe_projects['knx'] = '../build/xml'
+#	html_extra_path = ['../build/html']
