1321d62e400300d15c8826c1655a2a61819e040b
Thomas Kunze <thomas.kunze@gmx.com>
2019-07-02 23:54:31 +0200

    try an other way to create the memory reference point

--- src/knx/platform.h
+++ src/knx/platform.h
@@ -6,9 +6,10 @@
 
 class Platform
 {
-public:
+  public:
+    Platform();
     virtual uint32_t currentIpAddress() = 0;
-    virtual uint32_t currentSubnetMask()  = 0;
+    virtual uint32_t currentSubnetMask() = 0;
     virtual uint32_t currentDefaultGateway() = 0;
     virtual void macAddress(uint8_t* data) = 0;
 
@@ -16,26 +17,27 @@ public:
     virtual void restart() = 0;
     virtual void fatalError() = 0;
     virtual void mdelay(uint32_t millis) = 0;
-    
+
     virtual void setupMultiCast(uint32_t addr, uint16_t port) = 0;
     virtual void closeMultiCast() = 0;
     virtual bool sendBytes(uint8_t* buffer, uint16_t len) = 0;
     virtual int readBytes(uint8_t* buffer, uint16_t maxLen) = 0;
-    
+
     virtual void setupUart() = 0;
     virtual void closeUart() = 0;
     virtual int uartAvailable() = 0;
     virtual size_t writeUart(const uint8_t data) = 0;
-    virtual size_t writeUart(const uint8_t *buffer, size_t size) = 0;
+    virtual size_t writeUart(const uint8_t* buffer, size_t size) = 0;
     virtual int readUart() = 0;
-    virtual size_t readBytesUart(uint8_t *buffer, size_t length) = 0;
-        
+    virtual size_t readBytesUart(uint8_t* buffer, size_t length) = 0;
+
     virtual uint8_t* getEepromBuffer(uint16_t size) = 0;
     virtual void commitToEeprom() = 0;
-    
+
     virtual uint8_t* memoryReference();
     virtual uint8_t* allocMemory(size_t size);
     virtual void freeMemory(uint8_t* ptr);
-protected:
+
+  protected:
     uint8_t* _memoryReference = 0;
 };
