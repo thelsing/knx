24834d0f513cbf22720b9ee1866cc67230e162ac
Thomas Kunze <thomas.kunze@gmx.com>
2019-07-15 21:23:51 +0200

    run code through clang-format

--- src/knx/tpuart_data_link_layer.h
+++ src/knx/tpuart_data_link_layer.h
@@ -3,24 +3,23 @@
 #include <stdint.h>
 #include "data_link_layer.h"
 
-#define MAX_KNX_TELEGRAM_SIZE            263
-#define BYTE_TIMEOUT                     3           //milli seconds
-#define CONFIRM_TIMEOUT                  500         //milli seconds
+#define MAX_KNX_TELEGRAM_SIZE 263
 
-
-class TpUartDataLinkLayer: public DataLinkLayer
+class TpUartDataLinkLayer : public DataLinkLayer
 {
     using DataLinkLayer::_deviceObject;
-    using DataLinkLayer::_platform;
     using DataLinkLayer::_groupAddressTable;
-public:
+    using DataLinkLayer::_platform;
+
+  public:
     TpUartDataLinkLayer(DeviceObject& devObj, AddressTableObject& addrTab, NetworkLayer& layer,
-        Platform& platform);
+                        Platform& platform);
 
     void loop();
     void enabled(bool value);
     bool enabled() const;
-private:
+
+  private:
     bool _enabled = false;
     bool _waitConfirm = false;
     uint8_t* _sendBuffer = 0;
@@ -33,20 +32,22 @@ private:
     bool _isEcho = false;
     bool _isAddressed = false;
     bool _convert = false;
-    uint8_t _xorSum=0;
+    uint8_t _xorSum = 0;
     uint32_t _lastByteRxTime;
     uint32_t _waitConfirmStartTime;
 
-    struct _tx_queue_frame_t{
-        uint8_t *data;
+    struct _tx_queue_frame_t
+    {
+        uint8_t* data;
         uint16_t length;
-        _tx_queue_frame_t *next;
+        _tx_queue_frame_t* next;
     };
 
-    struct _tx_queue_t {
-      _tx_queue_frame_t *front=NULL;
-      _tx_queue_frame_t *back=NULL;
-    }_tx_queue;
+    struct _tx_queue_t
+    {
+        _tx_queue_frame_t* front = NULL;
+        _tx_queue_frame_t* back = NULL;
+    } _tx_queue;
 
     void addFrameTxQueue(CemiFrame& frame);
     bool isTxQueueEmpty();
@@ -58,4 +59,3 @@ private:
     void resetChip();
     void stopChip();
 };
-
