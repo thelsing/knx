5577774ef93818fef5ef4527232b7a80a701c557
Thomas Kunze <thomas.kunze@gmx.com>
2019-09-02 22:04:07 +0200

    print warning on DPT *.0

--- src/knx/dpt.cpp
+++ src/knx/dpt.cpp
@@ -1,11 +1,16 @@
 #include "dpt.h"
 
+#include "bits.h"
+
 Dpt::Dpt()
 {}
 
 Dpt::Dpt(short mainGroup, short subGroup, short index /* = 0 */)
     : mainGroup(mainGroup), subGroup(subGroup), index(index)
-{}
+{
+    if (subGroup == 0)
+        println("WARNING: You used and invalid Dpt *.0");
+}
 
 bool Dpt::operator==(const Dpt& other) const
 {
