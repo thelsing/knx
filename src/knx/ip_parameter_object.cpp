4bec5b36a2da5dab9983c7068d035e3d1108ecab
nanosonde <2073569+nanosonde@users.noreply.github.com>
2019-12-16 16:01:47 +0100

    Fix compilation

--- src/knx/ip_parameter_object.cpp
+++ src/knx/ip_parameter_object.cpp
@@ -7,7 +7,7 @@
 #include "callback_property.h"
 
 //224.0.23.12
-#define DEFAULT_MULTICAST_ADDR 0xE000170CUL
+#define DEFAULT_MULTICAST_ADDR ((uint32_t)0xE000170C)
 
 IpParameterObject::IpParameterObject(DeviceObject& deviceObject, Platform& platform): _deviceObject(deviceObject),
     _platform(platform)
