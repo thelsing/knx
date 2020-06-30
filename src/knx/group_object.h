4cf3dab9a693c9ee0ab3b41ed2002407f4c35e4c
etrinh <etrinh@zdionline.net>
2020-06-30 22:40:19 +0200

    Allow lambda on platforms which support it, can be forced with HAS_FUNCTIONAL 0/1

--- src/knx/group_object.h
+++ src/knx/group_object.h
@@ -19,7 +19,21 @@ enum ComFlag
 
 class GroupObject;
 
-#ifdef __linux__
+#ifndef HAS_FUNCTIONAL
+# if defined(__linux__) || defined(ARDUINO_ARCH_ESP32) || defined(ARDUINO_ARCH_STM32)
+#  define HAS_FUNCTIONAL    1
+# elif defined(__has_include)
+#  if __has_include (<functional>)
+#   define HAS_FUNCTIONAL   1
+#  else
+#   define HAS_FUNCTIONAL   0
+#  endif
+# else
+#  define HAS_FUNCTIONAL   0
+# endif
+#endif
+
+#if HAS_FUNCTIONAL
 #include <functional>
 typedef std::function<void(GroupObject&)> GroupObjectUpdatedHandler;
 #else
