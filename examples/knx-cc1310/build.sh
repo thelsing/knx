3f4542181ba8640850ee0ae56467fbd7739884fa
Nanosonde <2073569+nanosonde@users.noreply.github.com>
2020-10-15 16:31:40 +0200

    Clean up

--- examples/knx-cc1310/build.sh
+++ examples/knx-cc1310/build.sh
@@ -1,2 +1,2 @@
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_PREFIX_PATH="~/ti/simplelink_cc13x0_sdk_4_10_02_04;~/ti/xdctools_3_51_03_28_core" -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchain-arm-none-eabi.cmake ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_PREFIX_PATH="~/ti/simplelink_cc13x0_sdk_4_10_02_04" -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchain-arm-none-eabi.cmake ..
 
