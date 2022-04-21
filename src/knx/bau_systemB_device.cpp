thelsing/knx

commit 9c620a013d350272a78cbc7b5f5efd7cb7903695

GroupObjects can communicate internally (#194)
* GroupObjects can communicate internally
- GroupObjects use callback infrastructure also during write to bus
- external and internal write of a GO can be handled identically
- needs #define INTERNAL_GROUPOBJECT

* FIX-Works now also if SMALL_GROUPOBJECT is not set

* Remove #ifdef INTERNAL_GROUPOBJECT

Co-authored-by: Waldemar Porscha <waldemar.porscha@sap.com>

src/knx/bau_systemB_device.cpp
@@ -69,8 +69,21 @@ void BauSystemBDevice::sendNextGroupTelegram()
         if (flag != ReadRequest && flag != WriteRequest)
             continue;
 
+        if (flag == WriteRequest) 
+        {
+#ifdef SMALL_GROUPOBJECT
+            GroupObject::processClassCallback(go);
+#else
+            GroupObjectUpdatedHandler handler = go.callback();
+            if (handler)
+                handler(go);
+#endif
+        }
         if (!go.communicationEnable())
+        {
+            go.commFlag(Ok);
             continue;
+        }
 
         SecurityControl goSecurity;
         goSecurity.toolAccess = false; // Secured group communication never uses the toolkey. ETS knows all keys, also the group keys.
@@ -113,14 +126,17 @@ void BauSystemBDevice::updateGroupObject(GroupObject & go, uint8_t * data, uint8
 
     memcpy(goData, data, length);
 
-    go.commFlag(Updated);
+    if (go.commFlag() != WriteRequest)
+    {
 #ifdef SMALL_GROUPOBJECT
-    GroupObject::processClassCallback(go);
+       GroupObject::processClassCallback(go);
 #else
-    GroupObjectUpdatedHandler handler = go.callback();
-    if (handler)
-        handler(go);
+        GroupObjectUpdatedHandler handler = go.callback();
+        if (handler)
+            handler(go);
 #endif
+    }
+    go.commFlag(Updated);
 }
 
 bool BauSystemBDevice::configured()

commit cae65d0f4892121638dbdf64898426eaf864278f

FIX issue introduced with #194 - if go handler writes to same go, value is not sent to bus anymore

src/knx/bau_systemB_device.cpp
@@ -128,15 +128,19 @@ void BauSystemBDevice::updateGroupObject(GroupObject & go, uint8_t * data, uint8
 
     if (go.commFlag() != WriteRequest)
     {
+        go.commFlag(Updated);
 #ifdef SMALL_GROUPOBJECT
-       GroupObject::processClassCallback(go);
+        GroupObject::processClassCallback(go);
 #else
         GroupObjectUpdatedHandler handler = go.callback();
         if (handler)
             handler(go);
 #endif
     }
-    go.commFlag(Updated);
+    else
+    {
+        go.commFlag(Updated);
+    }
 }
 
 bool BauSystemBDevice::configured()