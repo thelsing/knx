088dd65c81aa1d79647c51ac04b7fc91aeb90d49
Thomas Kunze <thomas.kunze@gmx.com>
2025-02-23 00:53:38 +0100

    fix merge

--- src/knx/group_object.cpp
+++ src/knx/group_object.cpp
@@ -312,7 +312,6 @@ bool GroupObject::valueNoSend(const KNXValue& value, const Dpt& type)
         commFlag(Ok);
 
     return encodingDone;
-    return encodingDone;
 }
 
 bool GroupObject::valueNoSendCompare(const KNXValue& value, const Dpt& type)
